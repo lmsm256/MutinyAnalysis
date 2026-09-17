1f725ebdec918f0aa2103e53f18a009254498195
kademarcus <kmsp240@uky.edu>
2026-09-16 21:21:50 -0400

    Refactor model evaluation and update comments
    Updated model evaluation metrics to use fitstat for AIC and BIC calculations. Improved comments for clarity on model significance.

--- Regressions/testing_wald.R
+++ Regressions/testing_wald.R
@@ -8,46 +8,46 @@ coup_data$coup_fake <- rbinom(nrow(coup_data), size = 1, prob = 0.03)
 
 #if have nas in either, run this so test works
 # Clean your data first so both models use identical rows
-#clean_data <- subset(coup_data, !is.na(mutiny) & !is.na(coup))
+# cleaned_data <- coup_data[complete.cases(coup_data[, c("coup_fake", "polity2", "euds", "mutiny_fake")]), ] #again replace with actual variables
 
 
 #"naive model" without mutiny 
 reduced_model <- feglm(coup_fake ~ polity2 + euds, #replace with actual variables
-                     data = coup_data, 
+                     data = coup_data, #change to cleaned data if NAs not dealt with
                      family = "binomial",
                      cluster = ~ccode)
 reduced_model
 
 #actual model with mutiny
 full_model <- feglm(coup_fake ~ polity2 + euds + mutiny_fake, #replace with actual variables
-                  data = coup_data, 
+                  data = coup_data, #change to cleaned data if NAs not dealt with
                   family = "binomial",
                   cluster = ~ ccode)
 full_model
 
-# Wald test to test if a variable adds to predictive power (from fixest package)
+# Wald test to test if a variable is significant when controlling for others
 robust_test <- wald(full_model, keep = "mutiny_fake") #replace with munity, does test for us
 robust_test #if p < 0.05, significantly adds to model
 
 #AIC and BIC for models (want full model to have lower both of these for mutiny to have effect)
 #Extract AIC and BIC for both models
 
-#If AIC lower, generally improves model
-aic_reduced <- AIC(reduced_model)
-aic_full    <- AIC(full_model)
+#If AIC lower, generally better fitted model (from fitstat(since we use feglm))
+aic_reduced <- fitstat(reduced_model, "aic")$aic
+aic_full    <- fitstat(full_model, "aic")$aic
 
 if ((aic_full - aic_reduced) < 0){
-  print("Mutiny generally improves our model's predictive power")
+  print("Model with mutiny is preferred under AIC.")
 } else {
-  print("Mutiny DOES NOT improve the model under AIC")
+  print("Model with mutiny is not preferred under AIC.")
 }
 
 #BIC much more strict on added variables, so even better if this is lower
-bic_reduced <- BIC(reduced_model)
-bic_full    <- BIC(full_model)
+bic_reduced <- fitstat(reduced_model, "bic")$bic
+bic_full    <- fitstat(full_model, "bic")$bic
 
 if ((bic_full - bic_reduced) < 0) {
-  print("Mutiny heavily improves our model's predictive power")
+  print("Model with mutiny is preferred under BIC.")
 } else {
-  print("Mutiny DOES NOT improve the model under BIC")
+  print("Model with mutiny is not preferred under BIC.")
 }
